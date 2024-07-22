963957b555d91daac0a3da75f1b6615cd2dd86fc
alex <alex7deweloper@gmail.com>
2024-07-22 12:15:04 +0300

    Created orders app, created models

--- myshop/orders/apps.py
+++ myshop/orders/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OrordersConfig(AppConfig):
+class OrdersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ororders'
+    name = 'orders'
